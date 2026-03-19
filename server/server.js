0fe29d63037a500d95cf5a3b7698cedbf019e459
Saima Khan <saima19@somaiya.edu>
2026-03-20 00:56:00 +0530

    server.js changes and env and gitignore

--- server/server.js
+++ server/server.js
@@ -1,16 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
-
+//mongodb+srv://saima_k:<db_password>@saima19.okw35gn.mongodb.net/?appName=Saima19
 app.use(cors());
 app.use(express.json());
 
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+// Test route
 app.get("/", (req, res) => {
   res.send("MealMatch API Running");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
